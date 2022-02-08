d175a838160317499314c663a0b3842aa0a6c305
Abdulhamit Kumru <abdulhamitkumru@gmail.com>
2022-02-08 19:00:59 +0300

    project restructured, refactored, docker files added

--- README.md
+++ README.md
@@ -1,19 +1,32 @@
-﻿# Tübitak Ethereum Faucet
+﻿# BAGETH Faucet
 
 Bu projede kullanıcı **MetaMask** cüzdanına bağlanır. Ardından cüzdanıyla ilgili bilgilerini görüp bir buton sayesinde içerisinde **Ethereum** bulunan bir cüzdandan her basışında 0.1 ETH olacak şekilde bir aktarım gerçekleştirilir.
 
-
-# Dependencies
+## Bağımlılıklar
 
 Bu projenin herhangi bir dependencies'i bulunmamaktadır.
 
-## Run
+## Proje Yapısı
 
-Aşağıdaki komutlarla projeyi çalıştırabilirsiniz:
+`/frontend` React Fronted  
+`/backend` Express Backend
 
+## Projeyi Çalıştırma
+
+Aşağıdaki komutlarla projeyi çalıştırabilirsiniz.  
+
+React Fronted
+
+```sh
+$ cd frontend
+$ npm install
+$ npm start
 ```
-npm install
-node server.js
-yarn watch
-npm start
+
+Express Backend
+
+``` sh
+$ cd backend
+$ npm install
+$ node server.js
 ```
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 const express = require('express'); //Line 1
-const Web3 = require('web3');
 const app = express(); //Line 2
 const port = process.env.PORT || 5000; //Line 3
-const money = '0.1';
+
+
+// Ethereum, Web3.js 
+const Web3 = require('web3');
+const ethAmount = '0.1';
+const senderAddress = '0x15C2c14B416b2f5531CbCcdE364c048cf3F0f1dE';
+const privKey = '358547c8efd5381ad50841d73d5449d64a0c9f795070f8c54c34cbdf4c94969d';
 
 var bodyParser = require('body-parser')
 
 // create application/json parser
 var jsonParser = bodyParser.json()
-
-// create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // This displays message that the server running and listening to specified port
 app.listen(port,() => console.log(`Listening on port ${port}`)); //Line 6
@@ -22,30 +24,28 @@ app.use(function(req, res, next) {
 });
 
 
-app.post('/send_money', jsonParser, function (req, res) {
+app.post('/send_ether', jsonParser, function (req, res) {
     let web3 = new Web3(Web3.givenProvider || "https://eth.bag.org.tr/rpc");
     const toAddress = req.body.address;
-    const contractABI = req.body.contractAbi;
-    const contract = new web3.eth.Contract(contractABI, toAddress);
-    const senderAddress = '0x15C2c14B416b2f5531CbCcdE364c048cf3F0f1dE';
-    const privKey = '358547c8efd5381ad50841d73d5449d64a0c9f795070f8c54c34cbdf4c94969d';
-    const nonce = web3.utils.toHex(web3.eth.getTransactionCount(senderAddress, 'pending'));
 
     const tx = {
         from: senderAddress,
         to: toAddress,
-        value: web3.utils.toHex(parseInt(web3.utils.toWei(money, 'ether'))),
+        value: web3.utils.toHex(parseInt(web3.utils.toWei(ethAmount, 'ether'))),
         gasPrice: web3.utils.toHex(web3.utils.toWei('2', 'gwei')),
         gasLimit: web3.utils.toHex(21000),
     };
+
     web3.eth.transactionPollingTimeout = 30;
+
     if(toAddress){
         const signPromise = web3.eth.accounts.signTransaction(tx, privKey);
         signPromise.then((signedTx) => {
             const sentTx = web3.eth.sendSignedTransaction(signedTx.rawTransaction);
             console.log('Trying to send transaction...');
             sentTx.on("receipt", receipt => {
-                console.log('Transaction sent successfully \n' + senderAddress + ' to ' + toAddress + '\nAmount: ' + money + ' ETH');
+                console.log('Transaction sent successfully \n' + senderAddress + ' to ' + toAddress + '\nAmount: ' + ethAmount + ' ETH');
+
                 web3.eth.getBalance(toAddress)
                     .then((balanceInWei) => {
                         const balance = Web3.utils.fromWei(balanceInWei, 'ether');
